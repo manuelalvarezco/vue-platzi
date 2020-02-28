7a74826a575092402f20910098d096d789d26a61
Manuel Alvarez <alvarezbautista.luis@gmail.com>
2020-02-27 22:46:44 -0500

    Âtwo way data binding

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ new Vue({
     data() {
         return {
             name: 'Bitcoin',
+            symbol: 'BTC',
             img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
             changePercent: -10,
             price: 8400,
+            value: 0,
+            numero: 0,
+            color: 'F4F4F4',
             pricesWithDays: [
                 { day: 'Lunes', value: 8400 },
                 { day: 'Martes', value: 7900 },
@@ -18,9 +22,38 @@ new Vue({
             showPrices: false
         }
     },
+    computed: {
+        title() {
+            return `${this.name} + ${this.symbol}`
+        },
+        converterGauss() {
+            if (!this.numero) {
+                return 0
+            }
+
+            return (this.numero * (parseInt(this.numero) + 1)) / 2;
+        },
+        converterValue() {
+            if (!this.value) {
+                return 0
+            }
+
+            return this.value / this.price;
+        }
+    },
+
+    watch: {
+        showPrices(newVal, oldVal) {
+            console.log(newVal, oldVal);
+        }
+    },
     methods: {
         toogleShowPrices() {
             this.showPrices = !this.showPrices
+
+            this.color = this.color.split('')
+                .reverse().join('')
         }
     },
+
 })
